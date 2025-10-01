e6647f8807719837f34915127b050140c4f18b01
RasmusITT <rasmus.ojala1@gmail.com>
2025-10-01 12:40:02 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="et">
+<head>
+  <meta charset="UTF-8">
+  <title>Failide loetelu</title>
+</head>
+<body>
+  <h1>Minu failid</h1>
+  <ul>
+    <li><a href="flowers.csv">Lille csv</a></li>
+    <li><a href="banner.php">Banner</a></li>
+    <li><a href="kalkulaator.php">Kalkulaator</a></li>
+    <li><a href="kontakt.php">Kontakt</a></li>
+    <li><a href="ostukorv.php">Ostukorv</a></li>
+    <li><a href="projekt.php">Avaleht</a></li>
+    <li><a href="tooted.php"></a>Tooted</li>
+
+  </ul>
+</body>
+</html>
